5e81ef60eaf6daf9563090429485bfd13cdb9d3f
CodeByOmm <patelomc33@gmail.com>
2026-01-08 19:28:37 +0530

    converted images from .webp to png

--- src/components/blocks/changelog-section/v1_2_0.tsx
+++ src/components/blocks/changelog-section/v1_2_0.tsx
@@ -22,17 +22,17 @@ function v1_2_0() {
             <div className='fle-wrap flex items-center gap-4'>
               {/* vite */}
               <div className='flex items-center gap-1.5 rounded-md bg-amber-600/10 px-3 py-1 dark:bg-amber-400/10'>
-                <img src='/src/assets/images/vite-logo.webp' alt='Vite' className='h-4.5' loading='lazy' />
+                <img src='/src/assets/images/vite-logo.png' alt='Vite' className='h-4.5' loading='lazy' />
                 <span className='text-xs font-medium'>Vite</span>
               </div>
               {/* React */}
               <div className='flex items-center gap-1.5 rounded-md bg-sky-600/10 px-3 py-1 dark:bg-sky-400/10'>
-                <img src='/src/assets/images/react-logo.webp' alt='React' className='h-4.5' loading='lazy' />
+                <img src='/src/assets/images/react-logo.png' alt='React' className='h-4.5' loading='lazy' />
                 <span className='text-xs font-medium'>React</span>
               </div>
               {/* Angular */}
               <div className='bg-destructive/10 flex items-center gap-1.5 rounded-md px-3 py-1'>
-                <img src='/src/assets/images/angular-logo.webp' alt='Angular' className='h-4.5' loading='lazy' />
+                <img src='/src/assets/images/angular-logo.png' alt='Angular' className='h-4.5' loading='lazy' />
                 <span className='text-xs font-medium'>Angular</span>
               </div>
             </div>
--- src/components/blocks/changelog-section/v1_3_0.tsx
+++ src/components/blocks/changelog-section/v1_3_0.tsx
@@ -22,7 +22,7 @@ function v1_3_0() {
               <li>Automatically resolve dependency conflicts</li>
             </ul>
           </div>
-          <img src='/src/assets/images/image-1.webp' alt='Component Sync Demo' loading='lazy' />
+          <img src='/src/assets/images/image-1.png' alt='Component Sync Demo' loading='lazy' />
           <Accordion data={accordionDataV1_3_0} />
           <div className='flex flex-wrap items-center gap-4'>
             <div className='bg-primary/10 text-destructive rounded-md px-3 py-1 text-xs'>/v1/components/sync</div>
